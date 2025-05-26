2c05a29dc1897347cfd3f2096fa567d46d1b76ab
novinthen <42584998+novinthen@users.noreply.github.com>
2025-05-26 14:27:19 +0800

    Update app.py
    Fix TypeError: multiple values for argument 'options'

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ def upload_file():
     chrome_options.add_argument('--headless')
     chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument('--disable-dev-shm-usage')
-    driver = webdriver.Chrome(ChromeDriverManager().install(), options=chrome_options)
+    driver = webdriver.Chrome(executable_path=ChromeDriverManager().install(), options=chrome_options)
 
     results = []
     raw_html = []
